0a5bc49b89ae35e612ec06e4227a6533c75c35a0
Igor Frolov <lukztu1950@gmail.com>
2016-11-28 17:02:49 +0200

    Initial commit

---
+++ src/MainFolder/MainAlias.java
@@ -0,0 +1,10 @@
+package MainFolder;
+
+/**
+ * Created by Admin on 28.11.16.
+ */
+public class MainAlias {
+    public static void main(String[] args) {
+        System.out.println("Открытие Новой Торговой Программы");
+    }
+}
